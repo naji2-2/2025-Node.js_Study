71b6e3a031244f524dd7765284b67f58de318c0a
Najisu <jisusunny0225@gmail.com>
2025-03-19 11:36:55 +0900

    json 데이터 넘기기

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const express = require('express');
 // Express 애플리케이션 생성
 const app = express();
 
+app.use(express.json());
+
 // 라우팅 설정 루트 경로에 대한 요청 처리
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.post('/happy', (req, res) => {
+  res.send(req.body);
 });
 
 // 서버를 3000번 포트에서 실행
